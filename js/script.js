3aad6a6729a40515698064fdbad4933f3c73e432
Thiagodz7 <80972469+Thiagodz7@users.noreply.github.com>
2021-10-14 11:00:24 -0300

    versão 0.0.0.14.10.2021

---
+++ js/script.js
@@ -0,0 +1,73 @@
+class Todo{
+    constructor(){
+        this.totalTasks = document.querySelectorAll('.task').length
+    }
+    addTask(taskText){
+        /* console.log("funcionou"); */
+
+        //clonagem template(task hide):
+        let template = document.querySelector('.task').cloneNode(true);
+
+        //remover o hide(task hide):
+        template.classList.remove('hide');
+
+        //Manipular texto(task title):
+        let templateText = template.querySelector('.task-title');
+        templateText.textContent = taskText;
+        let list = document.querySelector('#tasks-container');
+
+        list.appendChild(template);
+        this.addEvents();
+        this.checkTasks('add')
+
+    }
+    checkTasks(command){
+        let msg = document.querySelector('#empty-tasks');
+        if(command ==='add'){
+            this.totalTasks +=1;
+        }
+        else if(command ==='remove'){
+            this.totalTasks -= 1;
+        }
+    }
+    removeTask(task){
+        //achar elemento:
+        let parentEl = task.parentElement;
+        //remover da lista
+        parentEl.remove();
+        this.checkTasks('remove')
+    }
+    completeTask(task){
+         task.classList.add('done');
+    }
+    addEvents(){
+        let removeBtns = document.querySelectorAll('.fa-trash');
+        let removeBtn = removeBtns[removeBtns.length - 1];
+        let doneBtns = document.querySelectorAll('.fa-check');
+        let doneBtn = doneBtns[doneBtns.length - 1];
+
+        //adicionar evento de remover a Task
+        removeBtn.addEventListener('click',function(){
+            todo.removeTask(this)
+        })
+        //adicionar ok (verde) quando tarefa finalizada:
+        doneBtn.addEventListener('click', function(){
+            todo.completeTask(this)
+        })
+    }
+}
+
+let todo = new Todo();
+let addBtn = document.querySelector("#add")
+
+addBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    let taskText = document.querySelector('#task');
+    if(taskText.value!="")
+    {
+     //console.log(taskText.value);
+     todo.addTask(taskText.value)
+    }else{
+        window.alert('Não pode estar vazio')
+    }
+})
